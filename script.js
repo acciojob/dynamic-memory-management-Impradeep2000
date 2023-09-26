94e2ecefdd984f9aacb3663a88222396c9473dbd
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-09-26 18:30:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,18 +6,34 @@ let elements = []; // Array to hold generated DOM elements
 // Function to generate DOM elements
 const generateElements = () => {
   // complete the function
+	for(let i = 0; i < 1000; i++) {
+		const el = document.createElement("div");
+		el.textContent = `Element ${i}`;
+
+		document.body.appendChild(el);
+	}
   updateMemoryUsage();
 };
 
 // Function to remove DOM elements
 const removeElements = () => {
   // complete the function
+	document.querySelectorAll("div").forEach(e => document.body.removeChild(e))
   updateMemoryUsage();
 };
 
 // Function to update memory usage display
 const updateMemoryUsage = () => {
   // Complete this function
+	if (window.performance && window.performance.memory) {
+	  const memoryInfo = window.performance.memory; 
+
+		document.getElementById("memory").textContent = `Memory usage: ${
+			Math.floor(memoryInfo.usedJSHeapSize / (1024 * 1024)) + " MB"
+		} / ${
+			Math.floor(memoryInfo.totalJSHeapSize / (1024 * 1024)) + " MB"
+		}`
+	}
 };
 
 // Add event listeners to buttons
